d29d9bb44052b11f8b1bf53d9e00d6f5680edcd4
Majd Taweel <42496516+MajdTaweel@users.noreply.github.com>
2020-10-26 07:11:52 +0300

    Add leaflet and ngx-leaflet packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    LeafletModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
